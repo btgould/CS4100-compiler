a38ebdd500c8a4db61016849bc44719f3f000299
Brendan Gould <brendan.gould314@gmail.com>
2024-02-01 15:38:42 -0700

    Update Java formatting

--- src/main/java/com/bgould/compiler/ADT/ReserveTable.java
+++ src/main/java/com/bgould/compiler/ADT/ReserveTable.java
@@ -6,125 +6,123 @@ import com.bgould.compiler.utils.StringUtils;
  * ReserveTable
  */
 public class ReserveTable {
-  /**
-   * Initializes a ReserveTable with enough internal storage to contain maxSize
-   * entries. This storage size cannot be changed later.
-   *
-   * @param maxSize The max number of rows in this ReserveTable
-   */
-  public ReserveTable(int maxSize) {
-    this.names = new String[maxSize];
-    this.codes = new int[maxSize];
-    this.count = 0;
-  }
+	/**
+	 * Initializes a ReserveTable with enough internal storage to contain maxSize
+	 * entries. This storage size cannot be changed later.
+	 *
+	 * @param maxSize The max number of rows in this ReserveTable
+	 */
+	public ReserveTable(int maxSize) {
+		this.names = new String[maxSize];
+		this.codes = new int[maxSize];
+		this.count = 0;
+	}
 
-  /**
-   * Appends a new row to the ReserveTable. Duplicates are allowed, and entries
-   * are not sorted in any way
-   *
-   * If the ReserveTable is already full, no item will be added and -1 is
-   * returned.
-   *
-   * @param name The name of the row in the ReserveTable
-   * @param code The code of the row
-   * @return The index of the added row in the ReserveTable (or -1, if the table
-   *     is full)
-   */
-  public int Add(String name, int code) {
-    if (count == names.length) {
-      System.err.println("Cannot add row to ReserveTable; already full");
-      return -1;
-    }
+	/**
+	 * Appends a new row to the ReserveTable. Duplicates are allowed, and entries
+	 * are not sorted in any way
+	 *
+	 * If the ReserveTable is already full, no item will be added and -1 is
+	 * returned.
+	 *
+	 * @param name The name of the row in the ReserveTable
+	 * @param code The code of the row
+	 * @return The index of the added row in the ReserveTable (or -1, if the table
+	 *     is full)
+	 */
+	public int Add(String name, int code) {
+		if (count == names.length) {
+			System.err.println("Cannot add row to ReserveTable; already full");
+			return -1;
+		}
 
-    names[count] = name;
-    codes[count] = code;
-    return count++;
-  }
+		names[count] = name;
+		codes[count] = code;
+		return count++;
+	}
 
-  /**
-   * Gets the code of a row with the given name (case-insensitive)
-   *
-   * @param name The name of the row to search for
-   * @return The code of the row in the ReserveTable, or -1 if row not found
-   */
-  public int LookupName(String name) {
-    int code = -1;
+	/**
+	 * Gets the code of a row with the given name (case-insensitive)
+	 *
+	 * @param name The name of the row to search for
+	 * @return The code of the row in the ReserveTable, or -1 if row not found
+	 */
+	public int LookupName(String name) {
+		int code = -1;
 
-    // linear search through names array
-    for (int i = 0; i < names.length; i++) {
-      if (name.equalsIgnoreCase(names[i])) {
-        code = codes[i];
-        break;
-      }
-    }
+		// linear search through names array
+		for (int i = 0; i < names.length; i++) {
+			if (name.equalsIgnoreCase(names[i])) {
+				code = codes[i];
+				break;
+			}
+		}
 
-    return code;
-  }
+		return code;
+	}
 
-  /**
-   * Gets the name of a row with the given code
-   *
-   * @param code The code of the row to search for
-   * @return The name of the row in the ReserveTable, or an empty String if row
-   *     not found
-   */
-  public String LookupCode(int code) {
-    String name = "";
+	/**
+	 * Gets the name of a row with the given code
+	 *
+	 * @param code The code of the row to search for
+	 * @return The name of the row in the ReserveTable, or an empty String if row
+	 *     not found
+	 */
+	public String LookupCode(int code) {
+		String name = "";
 
-    // linear search through codes array
-    for (int i = 0; i < codes.length; i++) {
-      if (code == codes[i]) {
-        name = names[i];
-      }
-    }
+		// linear search through codes array
+		for (int i = 0; i < codes.length; i++) {
+			if (code == codes[i]) {
+				name = names[i];
+			}
+		}
 
-    return name;
-  }
+		return name;
+	}
 
-  /**
-   * Pretty prints the contents of the ReserveTable. Empty rows are not printed
-   *
-   * @param filename The name of the file to print to.
-   */
-  public void PrintReserveTable(String filename) {
-    StringUtils.PrintToFile(filename, toString());
-  }
+	/**
+	 * Pretty prints the contents of the ReserveTable. Empty rows are not printed
+	 *
+	 * @param filename The name of the file to print to.
+	 */
+	public void PrintReserveTable(String filename) {
+		StringUtils.PrintToFile(filename, toString());
+	}
 
-  /**
-   * Represents the ReserveTable as a nicely formatted string.
-   *
-   * @return A String representation of the ReserveTable.
-   */
-  @Override
-  public String toString() {
-    // Calculate length needed for each column of the table
-    int idxColLen = 5;
-    int nameColLen = 4;
+	/**
+	 * Represents the ReserveTable as a nicely formatted string.
+	 *
+	 * @return A String representation of the ReserveTable.
+	 */
+	@Override
+	public String toString() {
+		// Calculate length needed for each column of the table
+		int idxColLen = 5;
+		int nameColLen = 4;
 
-    idxColLen = Math.max(idxColLen, Integer.valueOf(count).toString().length());
-    for (int i = 0; i < count; i++) {
-      nameColLen = Math.max(nameColLen, names[i].length());
-    }
+		idxColLen = Math.max(idxColLen, Integer.valueOf(count).toString().length());
+		for (int i = 0; i < count; i++) {
+			nameColLen = Math.max(nameColLen, names[i].length());
+		}
 
-    // Construct table
-    String repr = StringUtils.PadToLength("Index", idxColLen) + "\t" +
-                  StringUtils.PadToLength("Name", nameColLen) + "\t"
-                  + "Code";
-    for (int i = 0; i < count; i++) {
-      repr += "\n";
-      repr +=
-          StringUtils.PadToLength(Integer.valueOf(i).toString(), idxColLen) +
-          "\t" + StringUtils.PadToLength(names[i], nameColLen) + "\t" +
-          codes[i];
-    }
+		// Construct table
+		String repr = StringUtils.PadToLength("Index", idxColLen) + "\t" +
+		              StringUtils.PadToLength("Name", nameColLen) + "\t"
+		              + "Code";
+		for (int i = 0; i < count; i++) {
+			repr += "\n";
+			repr += StringUtils.PadToLength(Integer.valueOf(i).toString(), idxColLen) + "\t" +
+			        StringUtils.PadToLength(names[i], nameColLen) + "\t" + codes[i];
+		}
 
-    return repr;
-  }
+		return repr;
+	}
 
-  // Array of names in ReserveTable
-  private String[] names;
-  // Array of codes in ReserveTable
-  private int[] codes;
-  // Number of elements added to ReserveTable
-  private int count;
+	// Array of names in ReserveTable
+	private String[] names;
+	// Array of codes in ReserveTable
+	private int[] codes;
+	// Number of elements added to ReserveTable
+	private int count;
 }
--- src/main/java/com/bgould/compiler/utils/StringUtils.java
+++ src/main/java/com/bgould/compiler/utils/StringUtils.java
@@ -9,42 +9,41 @@ import java.io.OutputStreamWriter;
  * Class for utility functions related to Strings and printing
  */
 public class StringUtils {
-  /**
-   * Prints to the named file with the required error catching
-   *
-   * @param filename The file to print to
-   * @param toWrite The String to write to the file
-   */
-  public static void PrintToFile(String filename, String toWrite) {
-    try {
-      FileOutputStream outputStream = new FileOutputStream(filename);
-      OutputStreamWriter outputStreamWriter =
-          new OutputStreamWriter(outputStream);
-      BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+	/**
+	 * Prints to the named file with the required error catching
+	 *
+	 * @param filename The file to print to
+	 * @param toWrite The String to write to the file
+	 */
+	public static void PrintToFile(String filename, String toWrite) {
+		try {
+			FileOutputStream outputStream = new FileOutputStream(filename);
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+			BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-      bufferedWriter.write(toWrite);
-      bufferedWriter.newLine();
+			bufferedWriter.write(toWrite);
+			bufferedWriter.newLine();
 
-      bufferedWriter.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  /**
-   * Appends spaces as necessary to make the given input the desired length. If
-   * the input is longer than the desired length, no changes are made (i.e. this
-   * will never truncate a String)
-   *
-   * @param input The string to pad
-   * @param length The length to pad to
-   * @return A String starting with the given input and containing as many
-   *     trailing spaces as necessary to reach the given length
-   */
-  public static String PadToLength(String input, int length) {
-    int padLen = length - input.length();
-    padLen = Math.max(padLen, 0);
+	/**
+	 * Appends spaces as necessary to make the given input the desired length. If
+	 * the input is longer than the desired length, no changes are made (i.e. this
+	 * will never truncate a String)
+	 *
+	 * @param input The string to pad
+	 * @param length The length to pad to
+	 * @return A String starting with the given input and containing as many
+	 *     trailing spaces as necessary to reach the given length
+	 */
+	public static String PadToLength(String input, int length) {
+		int padLen = length - input.length();
+		padLen = Math.max(padLen, 0);
 
-    return input + " ".repeat(padLen);
-  }
+		return input + " ".repeat(padLen);
+	}
 }
